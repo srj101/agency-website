33cc545bf90d589eb2865a5939eb3975e4ae9749
srj101 <salim15-3117@diu.edu.bd>
2025-04-17 11:10:45 +0600

    init

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,17 +1,17 @@
-import type React from "react"
-import { SiteHeader } from "@/components/site-header"
-import { SiteFooter } from "@/components/site-footer"
+import type React from "react";
+import { SiteHeader } from "@/components/site-header";
+import { SiteFooter } from "@/components/site-footer";
 
 export default function SiteLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex min-h-screen flex-col mx-auto">
+    <div className="relative flex min-h-screen flex-col mx-auto my-0">
       <SiteHeader />
       <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
-  )
+  );
 }
